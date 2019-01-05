73fe62e20375b804161318a943eeb5461296ea1b
ToshiYoshioka <35719360+ToshiYoshioka@users.noreply.github.com>
2019-01-06 05:00:49 +0900

    スピード変数はなくてもアニメイトメソッドの引数で指定すればよいので修正

--- js/myScript.js
+++ js/myScript.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
   $('a[href^="#"]').click(function() {
-    var speed = 1000;
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top;
     $('body,html').animate({
       scrollTop:position
-      }, speed, 'swing');
+      }, 1000, 'swing');
     return false;
   });
   $(".aboutMeImg").hide().fadeIn(1000);
