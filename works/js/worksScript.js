4157cc7afe9441170849b7f14796d9bd1f34d21a
ToshiYoshioka <35719360+ToshiYoshioka@users.noreply.github.com>
2019-04-16 01:09:40 +0900

    jsonのデータを流し込む

--- works/js/worksScript.js
+++ works/js/worksScript.js
@@ -1,17 +1,42 @@
 $(document).ready(function(){
-  $('a[href^="#"]').click(function() {
-    var href = $(this).attr("href");
+  //ページ内リンクスクローラー
+  $('a[href^="#"]').click(function(){ //href^="#" → href属性の値が#で始まるアンカーを踏んだら…の意味
+    var href = $(this).attr('href');
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top;
     $('html,body').animate({
       scrollTop:position
       }, 1000, 'swing');
     return false;
+  });  
+  //ajaxでjsonデータを流し込む
+    $.ajax({
+      url: 'data.json', //読み込みたいファイルのパス
+      type: 'POST',  //通信方式にはpostメソッドを用いる
+      dataType: 'json' //データファイルをjsonとして定義
+    })
+    .done(function(data){ //ajax通信に成功して、jsonのデータをもらったら
+        $('#title').html(value.title); //案件名にtitleデータを流し込む
+        $.each(value.productSlide, function(imgIndex, imgValue){ //images のなかの各データを
+          $('#sliderImage').append($('<li>').append($('<img>', {src:imgValue}))); //<li>をappendして、さらに<img src=imgValue>をappendする
+        })
+        $('#assigned').html(value.assigned); //担当業務概要にassignedデータを流し込む
+        $('#teams').html(value.teams); //ポジション・チームにteamsデータを流し込む 
+        $('#period').html(value.period); //対応期間にperiodデータを流し込む
+        $('#language').html(value.language); //対応言語にlanguageデータを流し込む
+        $('#designTools').html(value.designTools); //制作ツールにdesignToolsデータを流し込む
+        $('#convTools').html(value.convTools); //コミュニケーションツールにconvToolsデータを流し込む
+        $('#comment').html(value.comment); //コメントにcommentデータを流し込む
+        //ajax通信は通常のリクエストの処理と並行して走るので、流し込みが終わってからbxsliderのオプションを指定する
+        //この順番で書かないと、画像の読み込み前にページャーを指定することになるのでうまくレンダリングされない
+        $('.worksHistory').bxSlider({ 
+          nextSelector:"a#next-btn",
+          prevSelector:"a#prev-btn",
+          nextText:'',
+          prevText:''
+        }) //.boxslider()の閉じカッコ
+      }) //.done()の閉じカッコ
+    .fail(function(){  //ajax通信に失敗した場合の処理
+      window.alert('読み込みエラー');
+    });
   });
-  $(".worksHistory").bxSlider({
-    nextSelector:"a#next-btn",
-    prevSelector:"a#prev-btn",
-    nextText:'',
-    prevText:''
-  });
-});
