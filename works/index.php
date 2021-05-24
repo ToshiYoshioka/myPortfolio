b6acffa80523acec3e1460985e69f0f09b69507f
ToshiYoshioka <35719360+ToshiYoshioka@users.noreply.github.com>
2021-05-25 01:59:40 +0900

    210525_googleAnalytics設定 #02_TagManagerを設置(UAでなくてGA4の方)

--- works/index.php
+++ works/index.php
@@ -48,7 +48,7 @@ $message = htmlspecialchars($message);
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','G-6TF86F09NF');
+  })(window,document,'script','dataLayer','GTM-P95LHN6');
   </script>
   <!-- End Google Tag Manager -->
 </head>
